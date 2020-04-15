048f24e7448cac1bdfb80d879d964c2c96f3b5e9
catherine57 <catherine.nicloux@hotmail.fr>
2020-04-15 11:19:49 +0200

    modification encodage fichier citations.js

--- src/citations.js
+++ src/citations.js
@@ -1,45 +1,45 @@
 
-// je suis un objet	
+// je suis un objet	d'objets
 module.exports = {
   
   citation1: {
     auteur: 'Henry David Thoreau',
-    citation: 'Si je suis venu au monde, ce n�est pas pour le transformer en un lieu o� il fasse bon vivre, mais pour y vivre.',
+    citation: 'Si je suis venu au monde, ce n\'est pas pour le transformer en un lieu où il fasse bon vivre, mais pour y vivre.',
    },
   citation2: {
     auteur: 'Henry David Thoreau',
-    citation: 'L��lite n�est, mat�riellement, ni plus avis�e ni meilleure que la masse.',
+    citation: 'L\'élite n\'est, matériellement, ni plus avisée ni meilleure que la masse.',
   },
   citation3: {
     auteur: 'Henry David Thoreau',
-    citation: 'En tuant le temps, on blesse l��ternit�.',
+    citation: 'En tuant le temps, on blesse l\'éternité.',
   },
   citation4: {
     auteur: 'Henry David Thoreau',
-    citation: 'On rougit d�abord de son crime et puis on s�y habitue.',
+    citation: 'On rougit d\'abord de son crime et puis on s\'y habitue.',
   },
   citation5: {
     auteur: 'Henry David Thoreau',
-    citation: 'Tu dois vivre dans le pr�sent, te lancer au-devant de chaque vague, trouver ton �ternit� � chaque instant.',
+    citation: 'Tu dois vivre dans le présent, te lancer au-devant de chaque vague, trouver ton éternité à chaque instant.',
   },
   citation6: {
     auteur: 'Ayn Rand',
-    citation: 'Ce que tu veux peut t\'appartenir, mais tu dois l\'accepter, y adh�rer de toute ton �me.',
+    citation: 'Ce que tu veux peut t\'appartenir, mais tu dois l\'accepter, y adhérer de toute ton âme.',
   },
   citation7: {
     auteur: 'Ayn Rand',
-    citation: 'Une plante peut obtenir sa nourriture du sol dans lequel elle pousse. Un animal doit chasser pour l�obtenir. L�homme doit la produire.',
+    citation: 'Une plante peut obtenir sa nourriture du sol dans lequel elle pousse. Un animal doit chasser pour l\'obtenir. L\'homme doit la produire.',
   },
   citation8: {
     auteur: 'Ayn Rand',
-    citation: 'Nous sommes tous fr�res sous la peau et j\'aimerais �corcher l\'humanit� pour le prouver.',
+    citation: 'Nous sommes tous frères sous la peau et j\'aimerais écorcher l\'humanité pour le prouver.',
   },
   citation9: {
     auteur: 'Ayn Rand',
-    citation: 'Le bonheur indique la r�ussite et la vie, la souffrance est un signal d�avertissement de d�faite et de mort.',
+    citation: 'Le bonheur indique la réussite et la vie, la souffrance est un signal d\'avertissement de défaite et de mort.',
   },
   citation10: {
     auteur: 'Ayn Rand',
-    citation: 'L�homme doit vivre pour son propre int�r�t, ne sacrifiant ni lui-m�me aux autres, ni les autres � lui-m�me. Vivre pour son propre int�r�t signifie que l�accomplissement de son propre bonheur est le plus haut but moral de l�homme.',
+    citation: 'L\'homme doit vivre pour son propre intérêt, ne sacrifiant ni lui-même aux autres, ni les autres à lui-même. Vivre pour son propre intérêt signifie que l\'accomplissement de son propre bonheur est le plus haut but moral de l\'homme.',
   }
 };
